d6fadd6ddb750cd4837aac393462e3a683634411
Weixiang Fang <wfang58@gmail.com>
2017-04-09 22:48:50 +0000

    updated results

--- project3/run_code.sh
+++ project3/run_code.sh
@@ -1,9 +1,13 @@
 mkdir -p output/CoinFlip
 
 # Coin Flip Speedup
-for i in {1..16}
+for j in {1..5}
 do
-	java CoinFlip $i 1000000000 >> ./output/CoinFlip/speedup.txt
+echo "cycle $j"
+for i in {1..32}
+do
+java CoinFlip $i 1000000000 >> ./output/CoinFlip/speedup.txt
+done
 done
 
 grep "Startup" ./output/CoinFlip/speedup.txt > ./output/CoinFlip/startup_time.txt
@@ -12,20 +16,28 @@ grep "Running" ./output/CoinFlip/speedup.txt > ./output/CoinFlip/speedup_time.tx
 mkdir -p output/SealedDES
 
 # SealedDES Scaleup
+for j in {1..5}
+do
+echo "cycle $j"
 java SealedDES 1 20 >> ./output/SealedDES/scaleup.txt
 java SealedDES 2 21 >> ./output/SealedDES/scaleup.txt
 java SealedDES 4 22 >> ./output/SealedDES/scaleup.txt
 java SealedDES 8 23 >> ./output/SealedDES/scaleup.txt
 java SealedDES 16 24 >> ./output/SealedDES/scaleup.txt
+done
 
 grep "Completed" ./output/SealedDES/scaleup.txt > ./output/SealedDES/scaleup_time.txt
 
 # DES extrapolation
-for i in {1..16}
+for j in {1..5}
+do
+echo "cycle $j"
+for i in {1..32}
 do
 	java SealedDES $i 21 >> ./output/SealedDES/extrapolation.txt
 	java SealedDES $i 22 >> ./output/SealedDES/extrapolation.txt
 	java SealedDES $i 23 >> ./output/SealedDES/extrapolation.txt
+done
 done
 
 grep "Completed" ./output/SealedDES/extrapolation.txt > ./output/SealedDES/extra_time.txt
